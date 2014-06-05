7f4b7a0f967eacf32ec50151a5e61eca2d53f1d5
rrv <admin@mail.ru>
2014-06-05 18:07:07 +0400

    editEvent

--- bookreestr-web/src/main/java/org/bm/ui/bean/DataBean.java
+++ bookreestr-web/src/main/java/org/bm/ui/bean/DataBean.java
@@ -11,7 +11,8 @@ import java.util.List;
  */
 public abstract class DataBean<T> {
 	protected List<T> items;
-
+	
+	
 	public List<T> getItems() {
 		return items;
 	}
--- bookreestr-web/src/main/java/org/bm/ui/bean/PersonBean.java
+++ bookreestr-web/src/main/java/org/bm/ui/bean/PersonBean.java
@@ -4,13 +4,22 @@
 package org.bm.ui.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 import org.bm.model.Person;
 import org.bm.service.impl.PersonBean1;
+import org.icefaces.ace.component.celleditor.CellEditor;
+import org.icefaces.ace.component.datatable.DataTable;
+import org.icefaces.ace.event.RowEditEvent;
+import org.icefaces.ace.model.table.RowState;
+import org.icefaces.ace.model.table.RowStateMap;
 
 /**
  * @author Black Moon
@@ -22,10 +31,65 @@ public class PersonBean extends DataBean<Person> implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	private boolean isNew = false;
+	private RowStateMap stateMap = new RowStateMap();
+
 	PersonBean1 pb = new PersonBean1();
 	
 	@PostConstruct
     public void init() {
         items = pb.getAll();
     }
+	
+	public void add(){
+		Person item = new Person(); 
+		items.add(item); 
+		
+		UIComponent u = FacesContext.getCurrentInstance().getViewRoot().findComponent("form2:gridPersons"); 
+		DataTable table = (DataTable)u;
+		 
+		RowState itemState = stateMap.get(item); 
+		
+		for (org.icefaces.ace.component.column.Column c : table.getColumns()) { 
+			CellEditor editor = c.getCellEditor(); 
+			
+			if (editor != null)			
+				itemState.addActiveCellEditor(editor);			
+		}
+		
+		isNew = true;
+	}
+	
+	public void edit(RowEditEvent e){
+		Person item = new Person();
+	}
+	
+	public void editCancel(RowEditEvent e){
+		if (isNew)
+		{
+			
+		}
+	}
+	
+	public void delete(ActionEvent e) {
+		List<?> selected = stateMap.getSelected();
+		for (Object o : selected)
+		{
+			items.remove(o);
+		}
+		selected.clear();
+	}
+	
+	
+	public select() {
+		
+	}
+	
+	public RowStateMap getStateMap() {
+		return stateMap;
+	}
+
+	public void setStateMap(RowStateMap stateMap) {
+		this.stateMap = stateMap;
+	}
 }
